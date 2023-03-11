b5d1425e2027c0d02cb958db0b324bc9cbb6fa64
andysush <sushkoandy1@gmail.com>
2023-03-11 09:58:44 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryDivEl = document.querySelector('.gallery');
+let makeImgMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+});
 
+galleryDivEl.insertAdjacentHTML('afterbegin', makeImgMarkup.join(''));
+
+new SimpleLightbox('.gallery__item', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
